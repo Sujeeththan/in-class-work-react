6cf16c90bbefec705961d05f77ee4c86156209e8
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-17 08:00:11 +0530

    onClick function added

--- src/components/StudentProfile.jsx
+++ src/components/StudentProfile.jsx
@@ -1,7 +1,17 @@
-function StudentProfile() {
+function StudentProfile({id , name , email , course , cohort , skills= true, github , onSelect}) {
     return(
         <>
-
+         <div className="profile-container">
+            <p>id: {id}</p>
+            <p>name: {name}</p>
+            <p>email: {email}</p>
+            <p>course: {course}</p>
+            <p>cohort: {cohort}</p>
+            <p>skills: {skills ? "HTML" : "CSS" }</p>
+            <p>github: {github}</p>
+            <p>onSelect: {onSelect}</p>
+            <button className="button" onClick={onSelect}>Click</button>
+         </div>
         </>
     )
 }
