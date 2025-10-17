1847eb6c1ad0bf2db51d29a046dc47f932e3afc1
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-17 08:01:13 +0530

    StudentProfile  onSelect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import './App.css'
-import Header from './components/Header'
-import StudentProfile from './cimomponents/StudentProfile'
-import Footer from './components/Footer'
-
+import "./App.css";
+import Header from "./components/Header";
+import StudentProfile from "./components/StudentProfile";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
+      <Header />
+      <StudentProfile
+        id="1"
+        name="Jey"
+        email="jey@gmail.com"
+        course="MERN Stack Development"
+        cohort="Cohort- 03"
+        skills={["HTML", "CSS", "JavaScript", "Reac"]}
+        github="https://github.com/jey"
+        onSelect={() => alert("Profile: Jey’s profile")}
+      />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
