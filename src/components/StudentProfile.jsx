f2fdceab30bb815a8392427f50a93169ec3efc4f
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-19 07:44:03 +0530

    skills change

--- src/components/StudentProfile.jsx
+++ src/components/StudentProfile.jsx
@@ -7,7 +7,7 @@ function StudentProfile({id , name , email , course , cohort , skills= true, git
             <p>email: {email}</p>
             <p>course: {course}</p>
             <p>cohort: {cohort}</p>
-            <p>skills: {skills ? "HTML" : "CSS" }</p>
+            <p>skills: {skills }</p>
             <p>github: {github}</p>
             <p>onSelect: {onSelect}</p>
             <button className="button" onClick={onSelect}>Click</button>
